2af5ea318cee4f4e79a69f3feb6fe030d0d0de92
Tyler VanNurden <tyler.vannurden@colorado.edu>
2024-04-13 16:05:39 -0600

    Complete live updates for apt price changes

--- app/javascript/channels/front_stats_channel.js
+++ app/javascript/channels/front_stats_channel.js
@@ -3,26 +3,36 @@ import consumer from "./consumer"
 consumer.subscriptions.create({ channel: "FrontStatsChannel" }, {
   received(data) {
     console.log('received', data)
-    this.insertAptPrice(data['aptos']['usd'])
+    this.insertAptPrice(parseFloat(data['aptos']['usd'].toFixed(2)))
+    this.insert24HChange(data['aptos']['usd_24h_change'])
+    this.insert24HVolume(data['aptos']['usd_24h_vol'])
   },
 
   insertAptPrice(price) {
-    const element = document.querySelector("#apt-price")
-    element.textContent = `$ ${price}`
+    const el = document.querySelector("#apt-price")
+    el.textContent = `$ ${price}`
   },
 
-  appendLine(data) {
-    const html = this.createLine(data)
-    const element = document.querySelector("[data-chat-room='Best Room']")
-    element.insertAdjacentHTML("beforeend", html)
+  insert24HChange(change) {
+    const el = document.querySelector('#apt-24h-change')
+
+    el.textContent = parseFloat(change.toFixed(2)) + '%'
+
+    if (change > 0) {
+      // positive, so green color
+      el.classList.remove('text-rose-600')
+      el.classList.add('text-green-600')
+    } else {
+      // negative, so red color
+      el.classList.remove('text-green-600')
+      el.classList.add('text-rose-600')
+    }
   },
 
-  createLine(data) {
-    return `
-      <article class="chat-line">
-        <span class="speaker">${data["sent_by"]}</span>
-        <span class="body">${data["body"]}</span>
-      </article>
-    `
+  insert24HVolume(change) {
+    const el = document.querySelector('#apt-24h-volume')
+
+    el.textContent = '$ ' + parseFloat((change / 1000000).toFixed(2)).toLocaleString() + 'M'
+
   }
 })
