7d0800683fad7e440c90e25a1eec4f4dfe075d02
Tyler VanNurden <tyler.vannurden@colorado.edu>
2024-04-13 14:21:16 -0600

    Add action cable into UI

---
+++ app/javascript/channels/front_stats_channel.js
@@ -0,0 +1,23 @@
+import consumer from "./consumer"
+
+consumer.subscriptions.create({ channel: "FrontStatsChannel" }, {
+  received(data) {
+    console.log('received')
+    // this.appendLine(data)
+  },
+
+  appendLine(data) {
+    const html = this.createLine(data)
+    const element = document.querySelector("[data-chat-room='Best Room']")
+    element.insertAdjacentHTML("beforeend", html)
+  },
+
+  createLine(data) {
+    return `
+      <article class="chat-line">
+        <span class="speaker">${data["sent_by"]}</span>
+        <span class="body">${data["body"]}</span>
+      </article>
+    `
+  }
+})
--- config/importmap.rb
+++ config/importmap.rb
@@ -5,3 +5,7 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
+# pin "vue" # @3.4.21
+
+pin "@rails/actioncable", to: "@rails--actioncable.js" # @7.1.3
+pin_all_from "app/javascript/channels", under: "channels"
