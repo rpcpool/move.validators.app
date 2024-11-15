6394fb77244d3d4cddd2ed09808822178a97391f
Tyler VanNurden <tyler.vannurden@colorado.edu>
2024-11-15 07:06:37 -0700

    Update network_shardable.rb

--- config/initializers/network_shardable.rb
+++ config/initializers/network_shardable.rb
@@ -2,7 +2,7 @@
 # require Rails.root.join('lib/extensions/network_shardable/table_name_helper')
 # require Rails.root.join('lib/extensions/network_shardable/network_shardable')
 # require Rails.root.join('lib/extensions/network_shardable/migration_helpers')
-require Rails.root.join('lib/extensions/network_shardable/*')
+require Rails.root.join('lib/extensions/network_shardable/**/*.rb')
 
 # Include MigrationHelpers in ActiveRecord::Migration
 ActiveSupport.on_load(:active_record) do
