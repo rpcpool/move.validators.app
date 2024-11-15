570c42a534deeb73983f856a45302585c827c697
Tyler VanNurden <tyler.vannurden@colorado.edu>
2024-11-15 10:20:34 -0700

    Change back to module because of `include`

--- lib/extensions/network_sharding/migration_helpers.rb
+++ lib/extensions/network_sharding/migration_helpers.rb
@@ -1,6 +1,6 @@
 module Extensions
   module NetworkSharding
-    class MigrationHelpers
+    module MigrationHelpers
       def create_sharded_table(model_class_or_name, *args, **options, &block)
         model_class_name = model_class_or_name.is_a?(Symbol) ? model_class_or_name.to_s : model_class_or_name.name.tableize
         table_name = NetworkSharding::TableNameHelper.sharded_table_name(model_class_name)
