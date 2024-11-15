4b6cf71c20d2681a969cf78ffbdf8fba7009315d
Tyler VanNurden <tyler.vannurden@colorado.edu>
2024-11-15 15:41:59 -0700

    Add sidekiq into deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,3 +23,38 @@ append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/syst
 # set :bundle_command, Whenever.bundler? ? "/usr/local/bin/bundle exec" : ""
 
 set :passenger_environment_variables, { path: '/usr/sbin/passenger-status:$PATH' }
+
+
+# SIDEKIQ CONFIG
+set :sidekiq_roles, :sidekiq
+set :sidekiq_config, File.join(current_path, 'config', 'sidekiq.yml').to_s
+
+namespace :sidekiq do
+  desc 'Stop sidekiq (graceful shutdown, put unfinished tasks back to Redis)'
+  task :stop do
+    on roles :sidekiq do |_role|
+      execute :systemctl, '--user', 'stop', :sidekiq
+    end
+  end
+
+  desc 'Start sidekiq'
+  task :start do
+    on roles :sidekiq do |_role|
+      execute :systemctl, '--user', 'start', :sidekiq
+    end
+  end
+
+  desc 'Restart sidekiq'
+  task :restart do
+    on roles(:sidekiq), in: :sequence, wait: 5 do
+      execute :systemctl, '--user', 'restart', :sidekiq
+    end
+  end
+
+  desc "Quiet sidekiq (stop fetching new tasks from Redis)"
+  task :quiet do
+    on roles(:sidekiq) do
+      execute :systemctl, '--user', 'kill -s TSTP', :sidekiq
+    end
+  end
+end
