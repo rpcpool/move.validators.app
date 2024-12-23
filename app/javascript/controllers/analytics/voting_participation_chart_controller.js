ed5585d430e81a326f3235e70191296bf90eee37
Tyler VanNurden <tyler.vannurden@colorado.edu>
2024-12-23 15:11:30 -0800

    Rename file to hope that it gets compiled sooner

--- app/javascript/controllers/analytics/voting_participation_chart_controller.js
+++ app/javascript/controllers/analytics/voting_participation_chart_controller.js
@@ -1,5 +1,5 @@
 // app/javascript/controllers/analytics/voting_participation_chart_controller.js
-import BaseAnalyticsChartController from "./base_analytics_chart_controller.js"
+import BaseAnalyticsChartController from "./aa_base_analytics_chart_controller.js"
 import {Chart} from "chart.js"
 
 export default class extends BaseAnalyticsChartController {
