ddcf0cfa7b4b6de3c336ac3fa4e98d7656af3692
Tyler VanNurden <tyler.vannurden@colorado.edu>
2024-12-23 14:47:05 -0800

    Add .js suffix to require statements

--- app/javascript/controllers/analytics/block_production_chart_controller-old.js
+++ app/javascript/controllers/analytics/block_production_chart_controller-old.js
@@ -1,4 +1,4 @@
-import BaseAnalyticsChartController from "./base_analytics_chart_controller"
+import BaseAnalyticsChartController from "./base_analytics_chart_controller.js"
 import {Chart} from "chart.js"
 
 export default class extends BaseAnalyticsChartController {
